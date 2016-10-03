cddc2497a827cab30aab696e5a0b39172ecbc50c
Esteban Ramirez <eramirez@grupow.com>
2016-10-03 08:44:04 -0500

    feat(ts): basic behavior

---
+++ app/typescripts/main.ts
@@ -0,0 +1,40 @@
+/// <reference path="definitions/jquery/jquery.d.ts" />
+
+//TODO: Refactor this shiet!!!
+$.fn.isOnScreen = function(){
+    
+    var win = $(window);
+    
+    var viewport: any = {
+        top : win.scrollTop(),
+        left : win.scrollLeft()
+    };
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+    
+    var bounds = this.offset();
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
+    
+    return ( !(
+      viewport.right < bounds.left || 
+      viewport.left > bounds.right || 
+      viewport.bottom < bounds.top || 
+      viewport.top > bounds.bottom ));
+    
+};
+
+//TODO: apply debounce on resize
+$(window).on('load scroll resize', function(){
+    $('.js-block').each(function(i) {
+      
+        var $el: any = $('.js-block').eq(i);
+        if( $el.isOnScreen() ){
+          $el.addClass('visible')
+        }else{
+          $el.removeClass('visible')
+        }
+    })
+});
+
+//TODO: preload images and make an intro!
